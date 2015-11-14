c929a95f0e0a85c631a33f0c047b8782c09b415a
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2015-11-14 07:04:05 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#+
+# Distutils script to build and install the GrainyPy modules. Invoke from the command
+# line in this directory as follows:
+#
+#     python3 setup.py install
+#
+# Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+#-
+
+import distutils.core
+
+distutils.core.setup \
+  (
+    name = "GrainyPy",
+    version = "0.5",
+    description = "Make High-Quality Images Look Grainy",
+    author = "Lawrence D’Oliveiro",
+    author_email = "ldo@geek-central.gen.nz",
+    py_modules = ["grainy"],
+    ext_modules =
+        [
+            distutils.core.Extension
+              (
+                name = "grainyx",
+                sources = ["grainyx.c"],
+                libraries = ["m"],
+              ),
+        ],
+  )
